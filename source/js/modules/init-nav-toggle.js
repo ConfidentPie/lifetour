b8a096cae97e1f013049d68f90e85087c6ad235b
yaroslav em <mr.emelyanenko@gmail.com>
2023-08-11 12:59:29 +0200

    bug fixing

--- source/js/modules/init-nav-toggle.js
+++ source/js/modules/init-nav-toggle.js
@@ -1,22 +1,49 @@
-let nav = document.querySelector('.header__nav');
-let toggle = document.querySelector('.header__nav-toggle');
+const nav = document.querySelector('.header__nav');
+const toggle = document.querySelector('.header__nav-toggle');
+const navLinks = document.querySelectorAll('.header__nav-link');
 let body = document.body;
 
 const initNavToggle = () => {
   nav.classList.remove('header__nav--nojs');
 
+  const closeMenu = () => {
+    nav.classList.remove('header__nav--opened');
+    nav.classList.add('header__nav--closed');
+    window.scrollLock.enableScrolling();
+    body.classList.remove('overlay');
+    document.removeEventListener('click', clickOutsideMenu);
+    document.removeEventListener('keydown', escapePress);
+  };
+
+  const clickOutsideMenu = (event) => {
+    if (!nav.contains(event.target) && !toggle.contains(event.target)) {
+      closeMenu();
+    }
+  };
+
+  const escapePress = (event) => {
+    if (event.key === 'Escape') {
+      closeMenu();
+    }
+  };
+
   toggle.addEventListener('click', function () {
     if (nav.classList.contains('header__nav--opened')) {
-      nav.classList.remove('header__nav--opened');
-      nav.classList.add('header__nav--closed');
-      window.scrollLock.enableScrolling();
-      body.classList.remove('overlay');
+      closeMenu();
     } else {
       nav.classList.remove('header__nav--closed');
       nav.classList.add('header__nav--opened');
       window.scrollLock.disableScrolling();
       body.classList.add('overlay');
+      document.addEventListener('click', clickOutsideMenu);
+      document.addEventListener('keydown', escapePress);
     }
+  });
+
+  navLinks.forEach((link) => {
+    link.addEventListener('click', () => {
+      closeMenu();
+    });
   });
 };
 
